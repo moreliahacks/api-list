6649acdf1f384b804325aeeb1a40d880231ca5f8
Sebastián Osorio <im@svzosorio.co>
2015-12-12 18:16:02 -0600

    Booljs boilerplate

---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict';
+var booljs = require('bool.js');
+
+// Here is where magic happens
+booljs('org.moreliahacks.list')
+    .setServerLoader('booljs-express')
+    .setDatabaseLoader('booljs-nomodel')
+    .run();
